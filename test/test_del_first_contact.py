6581d0c74d809694bae8972e7d602f23256e87f7
alyona_zaytseva <alenaomg1>
2019-01-12 16:10:55 +0300

    test_del_contact added and work

--- test/test_del_first_contact.py
+++ test/test_del_first_contact.py
@@ -1,6 +1,6 @@
 
 
-def delete_first_contact(app):
+def test_delete_first_contact(app):
     app.contact.open_home_page()
     app.session.login(username="admin", password="secret")
     app.contact.delete_first_contact()
