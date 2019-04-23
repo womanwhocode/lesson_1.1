daab9c2a6af7e8035408f3d0e67bcaebbc9ba962
alyona_zaytseva <alenaomg1>
2019-04-23 16:07:21 +0300

    cashe methods added

--- fixture/group.py
+++ fixture/group.py
@@ -15,6 +15,7 @@ class GroupHelper:
         wd.find_element_by_name("new").click()
         self.fill_group_form(group)
         wd.find_element_by_name("submit").click()
+        self.group_cache = None
 
     def fill_group_form(self, group):
         wd = self.app.wd
@@ -36,6 +37,7 @@ class GroupHelper:
         # submit deletion
         wd.find_element_by_name("delete").click()
         self.return_to_group_page()
+        self.group_cache = None
 
     def select_first_group(self):
         # select first group
@@ -62,22 +64,25 @@ class GroupHelper:
         wd.find_element_by_name("edit").click()
         self.fill_group_form(new_group_data)
         wd.find_element_by_name("update").click()
+        self.group_cache = None
 
     def count(self):
         wd = self.app.wd
         self.open_groups_page()
         return len(wd.find_elements_by_name("selected[]"))
 
-    def get_group_list(self):
-        wd = self.app.wd
-        self.open_groups_page()
-        groups = []
-        for element in wd.find_elements_by_css_selector("span.group"):
-            text = element.text
-            id = element.find_element_by_name("selected[]").get_attribute("value")
-            groups.append(Group(name=text, id=id))
-        return groups
+    group_cache = None
 
+    def get_group_list(self):
+        if self.group_cache is None:
+            wd = self.app.wd
+            self.open_groups_page()
+            self.group_cache = []
+            for element in wd.find_elements_by_css_selector("span.group"):
+                text = element.text
+                id = element.find_element_by_name("selected[]").get_attribute("value")
+                self.group_cache.append(Group(name=text, id=id))
+        return list(self.group_cache)
 
 
 
--- test/test_modify_group.py
+++ test/test_modify_group.py
@@ -3,14 +3,19 @@ from model.group import Group
 
 def test_modify_group_name(app):
     old_groups = app.group.get_group_list()
-    app.group.modify_first_group(Group(name="new name"))
+    group = Group(name="new name")
+    group.id = old_groups[0].id
+    app.group.modify_first_group(group)
     new_groups = app.group.get_group_list()
     assert len(old_groups) == len(new_groups)
+    old_groups[0] = group
+    assert sorted(old_groups, key=Group.id_or_max) == sorted(new_groups, key=Group.id_or_max)
+
 
 # comment
-def test_modify_group_header(app):
-    old_groups = app.group.get_group_list()
-    app.group.modify_first_group(Group(header="the newest header"))
-    new_groups = app.group.get_group_list()
-    assert len(old_groups) == len(new_groups)
+#def test_modify_group_header(app):
+ #   old_groups = app.group.get_group_list()
+   # app.group.modify_first_group(Group(header="the newest header"))
+    #new_groups = app.group.get_group_list()
+    #assert len(old_groups) == len(new_groups)
 
