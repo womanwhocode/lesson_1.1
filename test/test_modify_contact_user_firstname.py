5e6ec5b7e92391f90da0e1dc2b5d45f74860c7d4
alyona_zaytseva <alenaomg1>
2019-05-18 17:20:32 +0300

    test_modify_contact_user_firsname assert added

--- test/test_modify_contact_user_firstname.py
+++ test/test_modify_contact_user_firstname.py
@@ -5,6 +5,10 @@ from model.contact import Contact
 def test_modify_contact_user_firstname(app):
     app.contact.open_home_page()
     old_contacts = app.contact.get_contact_list()
-    app.contact.modify_first_contact(Contact(user_firstname="alena_new_4"))
+    contact = Contact(user_firstname="alena_new_4")
+    contact.id = old_contacts[0].id
+    app.contact.modify_first_contact(contact)
     new_contacts = app.contact.get_contact_list()
     assert len(old_contacts) == len(new_contacts)
+    old_contacts[0] = contact
+    assert sorted(old_contacts, key=Contact.id_or_max) == sorted(new_contacts, key=Contact.id_or_max)
